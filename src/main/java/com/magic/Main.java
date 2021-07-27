9d5e0e44578d0b7ca1da861327bb5731c05bdf93
m-a-g-i-c <dev.masteremove@gmail.com>
2021-07-27 22:09:32 +0300

    inital commit

---
+++ src/main/java/com/magic/Main.java
@@ -0,0 +1,9 @@
+package com.magic;
+
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello bro !");
+
+        System.out.println("Hy man!");
+    }
+}
